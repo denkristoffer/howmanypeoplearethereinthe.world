5cb3bae631dcaccc6ef1aa55d10060eb6b013579
Kristoffer <denkristoffer@users.noreply.github.com>
2022-08-14 13:19:57 +0000

    ✨ add credits (#549)

---
+++ src/components/credits.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Credits() {
+  return (
+    <>
+      <a href="https://kristoffer.is">made by kristoffer</a>
+
+      <style jsx>{`
+        a {
+          font-size: 15px;
+          margin: auto auto 5px auto;
+        }
+
+        @media (prefers-color-scheme: dark) {
+          a {
+            color: #4a92f7;
+          }
+        }
+      `}</style>
+    </>
+  );
+}
